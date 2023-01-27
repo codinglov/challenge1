6d6c4f515ebd061e6ecad5494fae34bb753a5093
codinglov <112770238+codinglov@users.noreply.github.com>
2023-01-26 19:09:54 -0600

    challenge1

--- 18-NoSQL/01-Activities/16-Stu_Models-Instance-Methods/Unsolved/models/Book.js
+++ 18-NoSQL/01-Activities/16-Stu_Models-Instance-Methods/Unsolved/models/Book.js
@@ -1,8 +1,18 @@
 const mongoose = require('mongoose');
 
 // TODO: Define a new schema named `bookSchema`
+const bookSchema = new mangoose.bookSchema({
+name: { type: String, required: true},
+totalStock: Number,
+lastAccessed: {type: Date, default: Date.now },
+})
 
 // TODO: Create a custom instance method named `getDiscount`
+bookSchema.methods.getDiscount = function (){
+  console.log('what is this', this.title
+
+  )
+} 
 
 // TODO: Create a model named `Book`
 
